43c0c788b80c2e7630d2adcf2879c78ecd792441
Zhenya <fedyai.eugen@gmail.com>
2018-05-14 19:01:38 +0300

    Complete task ninteen

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,43 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+const getVideo = function() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(LocalMediaStream => {
+      video.src = URL.createObjectURL(LocalMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error('You should probably allow your webcam to record', err);
+    });
+};
+
+const paintOnCanvas = function() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    console.log(pixels);
+  }, 16);
+};
+
+const takePhoto = function() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man">`;
+  strip.insertBefore(link, strip.firstChild);
+};
+ 
+getVideo();
+
+video.addEventListener('canplay', paintOnCanvas);
